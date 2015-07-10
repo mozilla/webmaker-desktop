884d0d74ee825e820b193f179a193e8b96cc6eca
Atul Varma <varmaa@gmail.com>
2015-07-10 17:28:48 -0400

    allow use of webmaker-core via npm link.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 var path = require('path');
+var fs = require('fs');
+
+var webmakerCorePath = fs.realpathSync(path.resolve(
+  __dirname,
+  'node_modules/webmaker-core'
+));
 
 module.exports = {
   entry: __dirname + '/src/main.jsx',
@@ -10,22 +16,25 @@ module.exports = {
     'react': 'React',
     'react/addons': 'React'
   },
+  // http://webpack.github.io/docs/troubleshooting.html#npm-linked-modules-doesn-t-find-their-dependencies
+  resolve: { fallback: path.join(__dirname, "node_modules") },
+  resolveLoader: { fallback: path.join(__dirname, "node_modules") },
   module: {
     loaders: [
       {
         test: /\.js$/,
         loaders: ['babel-loader'],
-        include: [path.resolve(__dirname, 'node_modules/webmaker-core/src'), path.resolve(__dirname, 'src')]
+        include: [path.join(webmakerCorePath, 'src'), path.resolve(__dirname, 'src')]
       },
       {
         test: /\.jsx$/,
         loaders: ['babel-loader', 'jsx-loader'],
-        include: [path.resolve(__dirname, 'node_modules/webmaker-core/src'), path.resolve(__dirname, 'src')]
+        include: [path.join(webmakerCorePath, 'src'), path.resolve(__dirname, 'src')]
       },
       {
         test: /\.json$/,
         loaders: ['json-loader'],
-        include: [path.resolve(__dirname, 'www_src'),  path.resolve(__dirname, 'node_modules')]
+        include: [path.join(webmakerCorePath, 'node_modules'), path.resolve(__dirname, 'www_src'),  path.resolve(__dirname, 'node_modules')]
       }
     ]
   }
